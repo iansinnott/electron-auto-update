88388a37a70fb20897b0682e5e030ca8b4a27125
Ian Sinnott <ian@iansinnott.com>
2021-07-19 12:17:09 +0800

    Configure build step

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+/**
+ *  __  __    _    ___ _   _
+ * |  \/  |  / \  |_ _| \ | |
+ * | |\/| | / _ \  | ||  \| |
+ * | |  | |/ ___ \ | || |\  |
+ * |_|  |_/_/   \_\___|_| \_|
+ *
+ * The main process. You're in a full node env here.
+ */
+
+import { app, ipcMain } from "electron";
